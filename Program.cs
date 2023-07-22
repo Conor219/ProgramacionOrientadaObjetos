b5ddcad46c3c38681352e31508e1011f8f37c341
Conor219 <daniel-attwell@hotmail.com>
2023-07-22 12:55:29 -0700

    Añadí un nuevo form de operaciones y su respectivo código.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProgramacionOrientadaObjetos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_Operaciones());
         }
     }
 }
