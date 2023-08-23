f485d972f4c28130ffee2fda129b59be0e88ab90
Conor219 <daniel-attwell@hotmail.com>
2023-08-23 15:22:42 -0700

    modf.

--- Form_Ilusiones2.cs
+++ Form_Ilusiones2.cs
@@ -27,19 +27,19 @@ namespace ProgramacionOrientadaObjetos
             int w = pictureBox1.Width;
             int h = pictureBox1.Height;
 
-            for (int i = 0; i < w/2; i += 10)
+            for (int i = 0; i < w / 2; i += 10)
             {
-                e.Graphics.DrawLine(colores[0], 0, i, i, h/2);
-                e.Graphics.DrawLine(colores[0], i, 0, w/2, i);
+                e.Graphics.DrawLine(colores[0], 0, i, i, h / 2);
+                e.Graphics.DrawLine(colores[0], i, 0, w / 2, i);
 
-                e.Graphics.DrawLine(colores[0], w/2 + i, 0, w/2, h/2 - i);
-                e.Graphics.DrawLine(colores[0], w, i, w - i, h/2);
+                e.Graphics.DrawLine(colores[0], w / 2 + i, 0, w / 2, h / 2 - i);
+                e.Graphics.DrawLine(colores[0], w, i, w - i, h / 2);
 
-                e.Graphics.DrawLine(colores[0], 0, h/2 + i, w/2 - i, h/2);
-                e.Graphics.DrawLine(colores[0], i, h, w/2, h - i);
+                e.Graphics.DrawLine(colores[0], 0, h / 2 + i, w / 2 - i, h / 2);
+                e.Graphics.DrawLine(colores[0], i, h, w / 2, h - i);
 
-                e.Graphics.DrawLine(colores[0], w/2, h/2 + i, w/2 + i , h);
-                e.Graphics.DrawLine(colores[0], w, h/2 + i, w/2 + i, h/2);
+                e.Graphics.DrawLine(colores[0], w / 2, h / 2 + i, w / 2 + i, h);
+                e.Graphics.DrawLine(colores[0], w, h / 2 + i, w / 2 + i, h / 2);
             }
         }
 
@@ -48,8 +48,8 @@ namespace ProgramacionOrientadaObjetos
             int w = pictureBox2.Width;
             int h = pictureBox2.Height;
 
-            for (int i = 0; i < w; i+= 10)
-            e.Graphics.DrawEllipse(colores[4], ((w-i)/2), ((h-i)/2), i, i);
+            for (int i = 0; i < w; i += 10)
+                e.Graphics.DrawEllipse(colores[4], ((w - i) / 2), ((h - i) / 2), i, i);
         }
 
         private void pictureBox3_Paint(object sender, PaintEventArgs e)
@@ -57,13 +57,13 @@ namespace ProgramacionOrientadaObjetos
             int w = pictureBox3.Width;
             int h = pictureBox3.Height;
 
-            for (int i = 0;i < w/2; i += 10)
+            for (int i = 0; i < w / 2; i += 10)
             {
                 e.Graphics.DrawLine(colores[2], w / 2, i, i, h - i);
                 e.Graphics.DrawLine(colores[2], w / 2, i, w - i, h - i);
                 e.Graphics.DrawLine(colores[2], i, h - i, w - i, h - i);
             }
-            
+
         }
 
         private void pictureBox4_Paint(object sender, PaintEventArgs e)
@@ -71,14 +71,14 @@ namespace ProgramacionOrientadaObjetos
             int w = pictureBox4.Width;
             int h = pictureBox4.Height;
 
-            for (int i = 0; i <= w/2 + 10; i += 10)
+            for (int i = 0; i <= w / 2 + 10; i += 10)
             {
-                e.Graphics.DrawLine(colores[3], i, 0, w/2, i);
-                e.Graphics.DrawLine(colores[3], i, h, w/2, h-i);
-                e.Graphics.DrawLine(colores[3], w/2 + i, 0, w/2, h/2-i);
-                e.Graphics.DrawLine(colores[3], w/2, h-i, w-i, h);
-                e.Graphics.DrawLine(colores[3], i, h-i, w/2-i, h/2-i);
-                e.Graphics.DrawLine(colores[3], w-i, h-i, w/2+i, h/2-i);
+                e.Graphics.DrawLine(colores[3], i, 0, w / 2, i);
+                e.Graphics.DrawLine(colores[3], i, h, w / 2, h - i);
+                e.Graphics.DrawLine(colores[3], w / 2 + i, 0, w / 2, h / 2 - i);
+                e.Graphics.DrawLine(colores[3], w / 2, h - i, w - i, h);
+                e.Graphics.DrawLine(colores[3], i, h - i, w / 2 - i, h / 2 - i);
+                e.Graphics.DrawLine(colores[3], w - i, h - i, w / 2 + i, h / 2 - i);
             }
         }
     }
